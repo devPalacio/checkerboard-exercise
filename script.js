f5db9bd7f34f2f0b48fe5c7ba6a44aac7c9279f3
Jay Palacio <jaypalacio@gmail.com>
2022-02-02 17:23:16 -0500

    checkerboard with cloneNode and :nth-child(even/odd)

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Your JS goes here
+let div = document.createElement('div')
+div.style.width = '11.1%'
+div.style.cssFloat = 'left'
+div.style.paddingBottom = '11.1%'
+for (let i = 0; i < 72; i++){
+  document.body.appendChild(div.cloneNode(true))
+}
+document
+  .querySelectorAll("div:nth-child(odd)")
+  .forEach((div) => (div.style.background = "red"));
+document
+  .querySelectorAll("div:nth-child(even)")
+  .forEach((div) => (div.style.background = "black"));
