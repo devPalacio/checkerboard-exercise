cb32a7d123bb18726f7c1384b0c39d7d53e44375
Jay Palacio <jaypalacio@gmail.com>
2022-02-03 07:34:46 -0500

    random colors!

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 // Your JS goes here
-let div = document.createElement('div')
-div.style.width = '11.1%'
-div.style.cssFloat = 'left'
-div.style.paddingBottom = '11.1%'
-for (let i = 0; i < 72; i++){
-  document.body.appendChild(div.cloneNode(true))
+let div = document.createElement("div");
+div.style.width = "11.1%";
+div.style.cssFloat = "left";
+div.style.paddingBottom = "11.1%";
+for (let i = 0; i < 72; i++) {
+  document.body.appendChild(div.cloneNode(true));
 }
-document
-  .querySelectorAll("div:nth-child(odd)")
-  .forEach((div) => (div.style.background = "red"));
-document
-  .querySelectorAll("div:nth-child(even)")
-  .forEach((div) => (div.style.background = "black"));
+
+// to make red/black
+// document
+//   .querySelectorAll("div:nth-child(odd)")
+//   .forEach((div) => (div.style.background = "red"));
+// document
+//   .querySelectorAll("div:nth-child(even)")
+//   .forEach((div) => (div.style.background = "black"));
+
+// random colors
+
+function randRGB() {
+  return Math.floor(Math.random() * 256);
+}
+let divs = document.querySelectorAll("div");
+divs.forEach(
+  (div) =>
+    (div.style.background = `rgb(${randRGB()},${randRGB()},${randRGB()})`)
+);
